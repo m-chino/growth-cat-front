5d8f8a20559f7bcb3e7552e75f65ef9d234971f8
RikiYoshino <r.yoshino@gxp.co.jp>
2019-04-05 15:46:53 +0900

    store作成

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+Vue.use(Vuex)
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+const store = new Vuex.Store({
+  state: {
+    msg: 'Hello'
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  store
 })
