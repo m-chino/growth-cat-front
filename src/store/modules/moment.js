7e96d7ebaa5d0dd521315d06f9e4635860bbe5f1
Tatsuya Okada <t.okada@gxp.co.jp>
2019-06-06 14:25:09 +0900

    コメント追加

--- src/store/modules/moment.js
+++ src/store/modules/moment.js
@@ -2,6 +2,7 @@ import {UPDATE_MOMENT, GET_NEW_MOMENT} from './../mutations'
 import axios from 'axios'
 /* eslint-disable */
 
+// ここをサーバのIPに合わせる
 const VUE_APP_API_URL_BASE =  'http://172.168.13.4:8080'
 
 export const Moments = {
